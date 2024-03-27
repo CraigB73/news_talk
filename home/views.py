6252e22d5ddf77515d57ba03a9333aa3786ea1f6
CraigB73 <craigbritton73@gmail.com>
2024-03-27 21:22:12 +0100

    Fix issue with templates and delete apps will add back later when needed

--- home/views.py
+++ home/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib import messages
 
+from django.http import HttpResponse
 # Create your views here.
 
+
+# def home_view(request):
+#   return HttpResponse("Home page")
 def home(request):
+  # template_name="home/index.html"
   context = {
-    'title': 'News Talk',
     'content': 'Place holder for content',
   }
-  return render(request, 'home.html', context)
+  return render(request, "home/index.html", context )
 
--- news_talk/urls.py
+++ news_talk/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('',include('home.urls', name='home-urls')),
+    path('', include('home.urls'), name='home'),
     path('admin/', admin.site.urls),
     
 ]
